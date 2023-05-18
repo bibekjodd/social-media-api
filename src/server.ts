77eb93c88a46d470b8b7ed07b191efae5cee44ed
BibeKJodd <bibekjodd@outlook.com>
2023-05-18 18:41:31 +0545

    initial-commit

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ connectDatabase();
 
 //
 app.listen(process.env.PORT, () => {
-    console.log(
-        `Server listening at http://localhost:${process.env.PORT}`.yellow
-    );
+  console.log(
+    `Server listening at http://localhost:${process.env.PORT}`.yellow
+  );
 });
