c0cfe384fa3239c8dd01cb109dc193ad6afccdd9
bibekjodd <bibekjodd@outlook.com>
2023-11-21 21:04:28 +0545

    add query posts api

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,6 +2,42 @@ import { db } from '@/config/database';
 import { CustomError } from '@/lib/custom-error';
 import { catchAsyncError } from '@/middlewares/catch-async-error';
 import { Posts } from '@/schema/post.schema';
+import { desc, eq } from 'drizzle-orm';
+
+type GetPostsQuery = {
+  page?: string;
+  pageSize?: string;
+  userId?: string;
+};
+export const getPosts = catchAsyncError<
+  unknown,
+  unknown,
+  unknown,
+  GetPostsQuery
+>(async (req, res) => {
+  let page = Number(req.query.page) || 1;
+  let pageSize = Number(req.query.pageSize) || 20;
+  if (page < 1) page = 1;
+  if (pageSize < 1) pageSize = 1;
+  if (pageSize > 50) pageSize = 20;
+  const offset = (page - 1) * pageSize;
+
+  const userId = req.query.userId;
+
+  const query = db
+    .select()
+    .from(Posts)
+    .limit(pageSize)
+    .offset(offset)
+    .orderBy(desc(Posts.createdAt));
+  if (!userId) {
+    const posts = await query;
+    return res.json({ posts });
+  }
+
+  const posts = await query.where(eq(Posts.userId, req.user.id));
+  return res.json({ posts });
+});
 
 type CreatePostBody = { caption?: string; image?: string };
 export const createPost = catchAsyncError<unknown, unknown, CreatePostBody>(
--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,6 +1,7 @@
-import { createPost } from '@/controllers/post.controller';
+import { createPost, getPosts } from '@/controllers/post.controller';
 import { isAuthenticatedUser } from '@/middlewares/auth';
 import { Router } from 'express';
 
 export const router = Router();
+router.get('/posts', getPosts);
 router.post('/create-post', isAuthenticatedUser, createPost);
