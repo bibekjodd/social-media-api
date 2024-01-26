2152c445c7923cc10fb9a7d1ab2f668b7685b72c
bibekjodd <bibekjodd@outlook.com>
2024-01-26 17:48:43 +0545

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -20,11 +20,11 @@ async function bootstrap() {
   app.enableCors({ origin: env.FRONTEND_URLS, credentials: true });
 
   const config = new DocumentBuilder()
-
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Social Media Api')
+    .setDescription(
+      'Social media api with full CRUD on Users, Posts, Likes and Comments<br>'
+    )
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
